54a282ba7a0e54d340508bb47685a4c2b36255c6
Eduardo Lemos Júnior <elemosjr@gmail.com>
2020-09-01 20:18:04 -0300

    add funcao gerar_capa do relatorio

---
+++ R/gerar_capa.R
@@ -0,0 +1,56 @@
+#' @title Gerar Capa do Relatório
+#'
+#' @description Retorna string com código em LaTeX para gerar a capa do relatório de consultorias
+#'
+#' @param titulo String com o título do relatório
+#' @param gerente String com o nome do gerente da consultoria
+#' @param assessores Vetor de caracteres com os assessores da consultoria
+#'
+#' @return character
+#'
+#' @examples
+#'  gerar_capa("Análise de alguma coisa", "Samuel Vianna Quintanilha", c("Ellen Beatriz Balan", "Larissa Rei Dos Santos"))
+#'
+#' @export
+
+gerar_capa <- function(titulo, gerente, assessores, bloco = FALSE)
+{
+  ret_ <- glue::glue(
+    "\\begin{titlepage}
+    \\begin{center}
+    
+    \\vspace*{4.5cm}
+    
+    \\begin{Large}
+    \\textbf{Relatório Final} \\\\ \n
+    \\end{Large}
+    
+    \\begin{LARGE}
+    \\textbf{`{titulo}`} \\\\ \n
+    \\end{LARGE}
+    
+    \\end{center}
+    
+    \\vspace*{3cm}
+    
+    \\flushleft{
+      \\begin{large}
+      \\textbf{Responsáveis:} \\\\ \n
+      \\end{large}
+      Gerente: `{gerente}` \\\\\\ \n
+      Assessores:
+      `{paste(assessores, '', collapse = '\\\\\\\\ \n')}`
+    }
+    
+    \\vfill
+    
+    \\begin{center}
+    Maringá \\\\ \n
+    \\today
+    \\end{center}
+    \\end{titlepage}",
+    .open = "`{", .close = "}`"
+  )
+  if(bloco) cat(ret_)
+  else return(ret_)
+}
